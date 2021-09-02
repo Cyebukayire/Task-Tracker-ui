5c0427a4b528c0494f1be3f65ad282f28267509b
Cyebukayire <peacecyebukayire@gmail.com>
2021-09-02 11:52:39 +0200

    Alter task

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -2,7 +2,7 @@
     <div @click="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' :
     '', 'task']">
         <h3>{{ task.text }}
-            <i @click="$emit('delete-task', task.id)" class="fas fa-times">x</i>
+            <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
         </h3>
         <p>{{ task.day }}</p>
     </div>
